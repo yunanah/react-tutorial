98e6d1a9ffa6aff99cab2170289b215500fae652
Yoorabaek <56185615+appletree821@users.noreply.github.com>
2020-11-03 11:01:12 +0900

    UserList에 항목을 추가/삭제 코드연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
+import InputSample from './InputSample';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
         id: 1,
         username: 'yura',
@@ -19,17 +36,40 @@ function App() {
         username: 'tata',
         email: 'tata@naver.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log(nextId.current); //4
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setInputs({
+      username:'',
+      email:''
+    });
+    setUsers([...users, user]);
+    // setUsers([users.concat(user)]); <- 다른 방법...!
+    // console.log(nextId.current); //4
     nextId.current += 1;
-  }
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
 
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate} 
+      />
+      <UserList users={users} onRemove={onRemove}/>
+    </>
   );
 }
 export default App;
