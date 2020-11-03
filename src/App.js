ec47ec586befbdbe06dbd5805c467f83614d6e70
Yoorabaek <56185615+appletree821@users.noreply.github.com>
2020-11-03 19:34:51 +0900

    useReducer을 활용해 App컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,75 +1,116 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useMemo, useCallback, useReducer } from 'react';
 import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import InputSample from './InputSample';
 
-function App() {
-  const [inputs, setInputs] = useState({
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+const initialState = {
+  inputs: {
     username: '',
     email: '',
-  });
-
-  const {username, email} = inputs;
-
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
-        id: 1,
-        username: 'yura',
-        email: 'bbyl6319@gmail.com',
-        active:true,
+      id: 1,
+      username: 'yura',
+      email: 'bbyl6319@gmail.com',
+      active:true,
     },
     {
-        id: 2,
-        username: 'kiki',
-        email: 'kiki@naver.com',
-        active:false,
+      id: 2,
+      username: 'kiki',
+      email: 'kiki@naver.com',
+      active:false,
     },
     {
-        id: 3,
-        username: 'tata',
-        email: 'tata@naver.com',
-        active:false,
+      id: 3,
+      username: 'tata',
+      email: 'tata@naver.com',
+      active:false,
     }
-  ]);
+  ]
+}
 
+function reducer(state, action) {
+  switch(action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs:{
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+          inputs: initialState.inputs,
+          users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id
+            ? {...user, active: !user.active}
+            : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {users} = state;
+  const {username, email} = state.inputs;
   const nextId = useRef(4);
+  const onChange = useCallback(e => {
+    const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
 
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setInputs({
-      username:'',
-      email:''
+  const onCreate = useCallback(e => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
-    setUsers([...users, user]);
-    // setUsers([users.concat(user)]); <- 다른 방법...!
-    // console.log(nextId.current); //4
     nextId.current += 1;
-  };
+  }, [username, email]);
 
-  const onRemove = id => {
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
 
-  const onToggle = id => {
-    setUsers(users.map(
-      user => user.id === id
-        ? {...user, active: !user.active}
-        : user
-    ));
-  };
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
@@ -77,13 +118,14 @@ function App() {
         username={username} 
         email={email} 
         onChange={onChange} 
-        onCreate={onCreate} 
+        onCreate={onCreate}
       />
       <UserList 
         users={users} 
-        onRemove={onRemove} 
         onToggle={onToggle} 
+        onRemove={onRemove}
       />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
