6fd4563130fbb972378b58cbe76715cb0bba8f94
Yoorabaek <56185615+appletree821@users.noreply.github.com>
2020-11-03 18:33:41 +0900

    Counter를 useReducer Hook 를 사용해서 수정

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+function reducer (state, action) {
+    switch(action.type) {
+        case 'INCREMENT': 
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            // return state; 이거나 아래 에러 쓰면 됨 .
+            throw new Error('unhandled action');
+    }
+}
 
 function Counter() {
-
-    const [number, setNumber] = useState(0);
-
+ 
+    const [number, dispatch] = useReducer(reducer, 0);
+    
     const increase = () => {
-        setNumber(number+1);
+        dispatch({
+            type: 'INCREMENT'
+        })
     }
 
     const decrease = () => {
-        setNumber(number-1);
+        dispatch({
+            type: 'DECREMENT'
+        })
     }
 
     return (
